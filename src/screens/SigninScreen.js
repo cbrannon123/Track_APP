3b14384062091e0b7666461b2baad08fe4ec6a50
Casey Brannon <cbrannon412@gmail.com>
2019-12-18 17:45:29 -0500

    app navigation

---
+++ src/screens/SigninScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, StyleSheet, Text} from 'react-native';
+
+const SigninScreen = () => {
+    return <View><Text style={{fontSize: 48 }}>signinscreeen</Text></View>
+};
+
+const styles = StyleSheet.create({});
+
+export default SigninScreen;
