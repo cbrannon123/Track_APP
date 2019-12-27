30ce591489f549f66b9dc625e6f71df40315acb8
Casey Brannon <cbrannon412@gmail.com>
2019-12-27 10:37:23 -0500

    signup nav

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://7f250f2c.ngrok.io'
+    baseURL: 'http://c346ead0.ngrok.io'
 });
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, TouchableOpacity } from 'react-native';
 import { Text, Input, Button } from 'react-native-elements';
 import Spacer from '../components/Spacer';
 import SigninScreen from './SigninScreen';
@@ -38,6 +38,11 @@ const SignupScreen = ({ navigation }) => {
             <Spacer>
                 <Button title="Sign Up" onPress={() => signup({ email, password })} />
             </Spacer>
+            <TouchableOpacity onPress={() => navigation.navigate('Signin')}>
+                <Spacer>
+                <Text style={styles.link}>Already have and account? Sign in instead</Text>
+                </Spacer>
+            </TouchableOpacity>
         </View>
     )
 };
@@ -58,8 +63,10 @@ const styles = StyleSheet.create({
         fontSize: 16,
         color: 'red',
         marginLeft: 15,
-        marginTop: 15
-        
+        marginTop: 1
+    },
+    link: {
+        color: 'blue'
     }
 });
 
