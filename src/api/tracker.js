38c3610ce310f4a36cd604f2e22d9e78896c0885
Casey Brannon <cbrannon412@gmail.com>
2020-01-30 14:54:07 -0500

    rendering tracklist

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { AsyncStorage } from 'react-native';
 
 const instance = axios.create({
-    baseURL: 'http://d009c9a9.ngrok.io'
+    baseURL: 'http://155e3049.ngrok.io'
 });
 
 instance.interceptors.request.use(
